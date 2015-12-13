d5461cf2500b22f9c98e02b2b31781181f28f6dc
venzozhang <594948794@qq.com>
2015-12-13 00:01:49 -0800

    test

--- build/build-status.py
+++ build/build-status.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 
 # Programs that are runnable.
-ns3_runnable_programs = ['build/scratch/ns3.22-scratch-simulator-debug', 'build/scratch/ns3.22-power-control-debug', 'build/scratch/subdir/ns3.22-subdir-debug']
+ns3_runnable_programs = ['build/scratch/ns3.22-power-control-debug', 'build/scratch/subdir/ns3.22-subdir-debug', 'build/scratch/ns3.22-scratch-simulator-debug']
 
 # Scripts that are runnable.
 ns3_runnable_scripts = []
--- build/ns3/config-store-config.h
+++ build/ns3/config-store-config.h
@@ -5,7 +5,7 @@
 
 #define PYTHONDIR "/usr/local/lib/python2.7/dist-packages"
 #define PYTHONARCHDIR "/usr/local/lib/python2.7/dist-packages"
-#define HAVE_PYTHON_H 1
+/* #undef HAVE_PYTHON_H */
 #define HAVE_GTK2 1
 #define HAVE_LIBXML2 1
 
